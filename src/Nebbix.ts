0670d447d55589a960210c679ae232c0e78ceba1
Bankole Emmanuel <mrbarnk1@gmail.com>
2021-04-04 23:51:29 +0100

    Updated README.MD & Added SendBtc to Functions

--- src/Nebbix.ts
+++ src/Nebbix.ts
@@ -15,42 +15,124 @@ export class Nebbix {
     public async initializeTransactions(body) {
         return this.sendRequest("/payment/initialize", {...body});
     }
+
+    /**
+     * paymentQuery
+     * GET request to query transaction;
+    */
+    
     public async paymentQuery (ref) {
         return this.sendRequest(`/payment/query?reference_code=${ref}`, {}, "GET");
     }
+
+    /**
+     * createBtcwallet
+    */
+    
     public async createBtcwallet (body) {
         return this.sendRequest("/wallet/btc/create", {...body});
     }
+
+    /**
+     * createBtcwalletAddress
+    */
+    
     public async createBtcwalletAddress (body) {
         return this.sendRequest("/wallet/btc/create_address", {...body});
     }
+
+    /**
+     * getBtcWallets
+    */
+    
     public async getBtcWallets () {
         return this.sendRequest(`/wallet/btc`, {}, "GET");
     }
+
+    /**
+     * getBtcWallet
+    */
+    
     public async getBtcWallet (wallet_id) {
         return this.sendRequest(`/wallet/btc/getWalletDetails/${wallet_id}`, {}, "GET");
     }
+
+    /**
+     * createLtcwallet
+    */
     
     public async createLtcwallet (body) {
         return this.sendRequest("/wallet/ltc/create", {...body});
     }
+
+    /**
+     * createLtcwalletAddress
+    */
+    
     public async createLtcwalletAddress (body) {
         return this.sendRequest("/wallet/ltc/create_address", {...body});
     }
+
+    /**
+     * getLtcWallet
+    */
+    
     public async getLtcWallet (wallet_id) {
         return this.sendRequest(`/wallet/ltc/get_wallets?wallet_id=${wallet_id}`, {}, "GET");
     }
+
+    /**
+     * createDogewallet
+    */
     
     public async createDogewallet (body) {
         return this.sendRequest("/wallet/doge/create", {...body});
     }
+
+    /**
+     * createDogewalletAddress
+    */
+    
     public async createDogewalletAddress (body) {
         return this.sendRequest("/wallet/doge/create_address", {...body});
     }
+
+    /**
+     * getDogeWallet
+    */
+    
     public async getDogeWallet (wallet_id) {
         return this.sendRequest(`/wallet/doge/get_wallets?wallet_id=${wallet_id}`, {}, "GET");
     }
-    
+
+    /**
+     * sendBtc
+    */
+
+    public sendBtc(body) {
+        return this.sendRequest("/wallet/btc/send", {...body});
+    }
+
+    /**
+     * sendLtc
+    */
+
+    public sendLtc(body) {
+        return this.sendRequest("/wallet/ltc/send", {...body});
+    }
+
+    /**
+     * sendDoge
+    */
+
+    public sendDoge(body) {
+        return this.sendRequest("/wallet/doge/send", {...body});
+    }
+    /**
+     * sendRequest
+     * Send POST or GET request to BASE_URL;
+    */
+     
     private async sendRequest (url, body, method = "POST") {
         if (method == "GET") {
             return await axios.get(`${BASE_URL}${url}`, {
