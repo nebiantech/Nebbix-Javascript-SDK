7704cd9503b33d6e080a80b0b1b7d6e0a77a3e49
Bankole Emmanuel <mrbarnk1@gmail.com>
2021-04-04 23:16:05 +0100

    First Upload

---
+++ index.ts
@@ -0,0 +1,3 @@
+const Nebbix = require("./src/Nebbix");
+let nebbix = new Nebbix("SUstLcEqsoNVxi7", "c3e45968-8b3f-42a6-8873-6fedf5194150");
+// nebbix.initialize("SUstLcEqsoNVxi7", "c3e45968-8b3f-42a6-8873-6fedf5194150");
---
+++ src/Nebbix.ts
@@ -0,0 +1,83 @@
+const axios = require("axios")
+const BASE_URL = "http://localhost:8000/userApi"
+
+export class Nebbix {
+    private userAccessToken = "";
+    private clientId = "";
+    
+    constructor(userAccessToken: string, clientId: string) {
+        this.userAccessToken = userAccessToken;
+        this.clientId = clientId;
+    }
+    /**
+     * initializeTransactions
+     */
+    public async initializeTransactions(body) {
+        return this.sendRequest("/payment/initialize", {...body});
+    }
+    public async paymentQuery (ref) {
+        return this.sendRequest(`/payment/query?reference_code=${ref}`, {}, "GET");
+    }
+    public async createBtcwallet (body) {
+        return this.sendRequest("/wallet/btc/create", {...body});
+    }
+    public async createBtcwalletAddress (body) {
+        return this.sendRequest("/wallet/btc/create_address", {...body});
+    }
+    public async getBtcWallets () {
+        return this.sendRequest(`/wallet/btc`, {}, "GET");
+    }
+    public async getBtcWallet (wallet_id) {
+        return this.sendRequest(`/wallet/btc/getWalletDetails/${wallet_id}`, {}, "GET");
+    }
+    
+    public async createLtcwallet (body) {
+        return this.sendRequest("/wallet/ltc/create", {...body});
+    }
+    public async createLtcwalletAddress (body) {
+        return this.sendRequest("/wallet/ltc/create_address", {...body});
+    }
+    public async getLtcWallet (wallet_id) {
+        return this.sendRequest(`/wallet/ltc/get_wallets?wallet_id=${wallet_id}`, {}, "GET");
+    }
+    
+    public async createDogewallet (body) {
+        return this.sendRequest("/wallet/doge/create", {...body});
+    }
+    public async createDogewalletAddress (body) {
+        return this.sendRequest("/wallet/doge/create_address", {...body});
+    }
+    public async getDogeWallet (wallet_id) {
+        return this.sendRequest(`/wallet/doge/get_wallets?wallet_id=${wallet_id}`, {}, "GET");
+    }
+    
+    private async sendRequest (url, body, method = "POST") {
+        if (method == "GET") {
+            return await axios.get(`${BASE_URL}${url}`, {
+                headers: {
+                        'Content-Type': 'application/json',
+                        'user-access-token': this.userAccessToken,
+                        'client-id': this.clientId
+                    }
+            }).then(e => e)
+                .catch(e => {
+                    const {response} = e;
+                    throw response.data;
+                })
+        }
+        return await axios.post(`${BASE_URL}${url}`, {...body}, {
+                headers: {
+                        'Content-Type': 'application/json',
+                        'user-access-token': this.userAccessToken,
+                        'client-id': this.clientId
+                    }
+            }).then(e => e)
+                .catch(e => {
+                    const {response} = e;
+                    throw response.data;
+                })
+    }
+
+}
+
+module.exports = Nebbix
